26fc0bb26c5affaec6c980499b9d82433ad5e95f
Bakhadirov-Jaloliddin-25 <jboxodirov4@gmail.com>
2025-01-09 23:38:35 +0500

    first commit

---
+++ tailwind.config.ts
@@ -0,0 +1,33 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        primary: "#F2CEDA",
+        btnprimary: "#D23166",
+        btnhover: "#FAFAFA",
+      },
+    },
+    container: {
+      center: true,
+      screens: {
+        sm: "600px",
+        md: "728px",
+        lg: "984px",
+        xl: "1040px",
+        "2xl": "1121px",
+      },
+      padding: "1rem",
+    },
+  },
+  plugins: [],
+} satisfies Config;
+
